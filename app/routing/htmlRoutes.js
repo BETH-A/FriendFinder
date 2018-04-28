c5295237bbeb1a70643341b172fb9edaf2d4896b
BETH-A <elizabeth78@me.com>
2018-04-27 22:27:38 -0400

    FriendFinder

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,15 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
 router.get('/', function(req, res) {
 	res.sendFile('home.html', { root: path.join(__dirname, '../public')});
 });
 
+// Displays survey
 router.get('/survey', function(req, res){
 	res.sendFile('survey.html', { root: path.join(__dirname, '../public')});
 });
