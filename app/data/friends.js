ac8aa02be1bdf22bccd897ef884031fec518f5c3
BETH-A <elizabeth78@me.com>
2018-04-29 11:15:42 -0400

    FriendFinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,55 @@
 //Create friends array of objects to store each friend's data
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
+        name:"Ian",
+        photo:"http://lorempixel.com/g/400/200/abstract/1",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Charlotte",
+        photo:"http://lorempixel.com/g/400/200/abstract/2",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Sean",
+        photo:"http://lorempixel.com/g/400/200/abstract/3",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Grace",
+        photo:"http://lorempixel.com/g/400/200/abstract/4",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Liam",
+        photo:"http://lorempixel.com/g/400/200/abstract/5",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Avery",
+        photo:"http://lorempixel.com/g/400/200/abstract/6",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Adam",
+        photo:"http://lorempixel.com/g/400/200/abstract/7",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Serena",
+        photo:"http://lorempixel.com/g/400/200/abstract/8",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Alex",
+        photo:"http://lorempixel.com/g/400/200/abstract/9",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },
+      {
+        name:"Eve",
+        photo:"http://lorempixel.com/g/400/200/abstract/10",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        }
 ];
 
 module.exports = friends;
