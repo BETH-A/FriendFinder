93c86aaa490eec9f77e7c3dfe3d395d989a76989
BETH-A <elizabeth78@me.com>
2018-05-01 18:03:20 -0400

    FriendFinder

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Allow files in public folder to be served static
+app.use(express.static(__dirname + '/app/public'));
+
+// Routers
+app.use(require('./app/routing/apiRoutes'));
+app.use(require('./app/routing/htmlRoutes'));
+
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
