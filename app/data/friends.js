8c1e10ac59d3e2b7ebb9fb8e266757234d15ca59
BETH-A <elizabeth78@me.com>
2018-05-03 17:59:30 -0400

    FriendFinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,47 +8,47 @@ var friends = [
       {
         name:"Charlotte",
         photo:"http://lorempixel.com/g/400/200/abstract/2",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[2, 4, 3, 1, 5, 4, 3, 2, 1, 5]
       },
       {
         name:"Sean",
         photo:"http://lorempixel.com/g/400/200/abstract/3",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[1, 5, 2, 2, 1, 5, 4, 1, 2, 5]
       },
       {
         name:"Grace",
         photo:"http://lorempixel.com/g/400/200/abstract/4",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[4, 2, 3, 5, 1, 2, 4, 4, 5, 1]
       },
       {
         name:"Liam",
         photo:"http://lorempixel.com/g/400/200/abstract/5",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[4, 4, 2, 1, 3, 4, 5, 2, 1, 3]
       },
       {
         name:"Avery",
         photo:"http://lorempixel.com/g/400/200/abstract/6",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[2, 2, 4, 5, 3, 2, 1, 4, 5, 3]
       },
       {
         name:"Adam",
         photo:"http://lorempixel.com/g/400/200/abstract/7",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[5, 4, 3, 4, 5, 1, 2, 5, 4, 1]
       },
       {
         name:"Serena",
         photo:"http://lorempixel.com/g/400/200/abstract/8",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[5, 1, 4, 2, 4, 1, 2, 4, 4, 1]
       },
       {
         name:"Alex",
         photo:"http://lorempixel.com/g/400/200/abstract/9",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[3, 4, 2, 4, 3, 2, 2, 4, 4, 3]
       },
       {
         name:"Eve",
         photo:"http://lorempixel.com/g/400/200/abstract/10",
-        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores:[3, 2, 4, 2, 3, 4, 4, 2, 2, 3]
         }
 ];
 
